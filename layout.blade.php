eb6eaa4661e7348fb315b63dfe71074ff66e0acc
Sid Kumai <kumais@Sid-iMac-local>
2013-06-24 15:59:43 +1000

    layout file points to default boostrap folder

--- layout.blade.php
+++ layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>@yield('title')</title>
-    <link href="{{ url(theme_path('css/cyborg/bootstrap.min.css')) }}" rel="stylesheet" media="screen">
+    <link href="{{ url(theme_path('css/default/bootstrap.min.css')) }}" rel="stylesheet" media="screen">
     <link href="{{ url(theme_path('css/style.css')) }}" rel="stylesheet" media="screen">
   </head>
   <body>
